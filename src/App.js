3d4557cbac00b94d69a23341e2c33a831899499b
And4Web <anands2807@gmail.com>
2022-02-21 13:47:51 -0800

    data from firestore retreived

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { collection } from "firebase/firestore";
+import { collection, doc, getDocs } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
 function App() {
@@ -8,12 +8,24 @@ function App() {
   const usersRef = collection(db, "users");
 
   useEffect(() => {
-    const getUsers = async () => {};
+    const getUsers = async () => {
+      const data = await getDocs(usersRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(users);
+    };
     getUsers();
   }, []);
   return (
     <div className="App">
       <h1>Hello...</h1>
+      {users.map((user) => {
+        return (
+          <div>
+            Name: {user.firstName} {user.lastName} Age: {user.age} Height:
+            {user.height} Weight: {user.weight}
+          </div>
+        );
+      })}
     </div>
   );
 }
