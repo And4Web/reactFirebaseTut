7400fc108997e6a53505fc441f22a8b2a2e8cd4c
And4Web <anands2807@gmail.com>
2022-02-21 14:16:04 -0800

    created new users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { collection, doc, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs, addDoc } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
+  const [newHeight, setNewHeight] = useState("");
+  const [newWeight, setNewWeight] = useState("");
   const usersRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersRef, {
+      Name: newName,
+      Age: newAge,
+      Height: newHeight,
+      Weight: newWeight,
+    });
+  };
 
   useEffect(() => {
     const getUsers = async () => {
@@ -17,12 +30,30 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <h1>Hello...</h1>
+      <input placeholder="Name" onChange={(e) => setNewName(e.target.value)} />
+      <input
+        type="number"
+        placeholder="Age"
+        onChange={(e) => setNewAge(e.target.value)}
+      />
+      <input
+        placeholder="Height"
+        onChange={(e) => setNewHeight(e.target.value)}
+      />
+      <input
+        placeholder="Weight"
+        onChange={(e) => setNewWeight(e.target.value)}
+      />
+
+      <button onClick={createUser}>Add User</button>
       {users.map((user) => {
         return (
           <div>
-            Name: {user.firstName} {user.lastName} Age: {user.age} Height:
-            {user.height} Weight: {user.weight}
+            {`Name: ${user.firstName} ${user.lastName}, 
+            Age: ${user.age}, 
+            Height: ${user.height}, 
+            Weight: ${user.weight},
+            key: ${user.id}`}
           </div>
         );
       })}
